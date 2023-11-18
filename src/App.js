997563b171d450e0c73a415da6bec0202b62c456
qrisma <nobel77.lee@gmail.com>
2023-11-18 21:50:26 +0900

    v1 : index를 키값으로 사용한 기본 버전

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState(["밥먹기", "코딩하기"]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addToList = () => {
+    // 이전 값 앞에 새로운 값 추가
+    setList((prevList) => {
+      // 새로운 항목이 추가될때마다,
+      // 항목 렌더링 시, 기존 항목의 key 값이 +1이 되기 때문에
+      // 모든 항목을 다시 그린다.
+      return [inputValue, ...prevList];
+    });
+    setInputValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+      <button onClick={addToList}>추가</button>
+      <ul>
+        {list.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+    </>
   );
 }
 
